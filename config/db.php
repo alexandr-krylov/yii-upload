e6d8599cd4d015eb197fbfeb2add09382c33dece
Alexandr Krylov <alexandr.krylov@gmail.com>
2024-02-18 14:18:06 +0600

    add countries page witch connects to db

--- config/db.php
+++ config/db.php
@@ -1,7 +1,12 @@
 <?php
 
+use yii\db\Connection;
+
 return [
-    'class' => 'yii\db\Connection',
+    'class' => Connection::class, //'yii\db\Connection',
+    'schemaMap' => [
+        'mysql' => \SamIT\Yii2\MariaDb\Schema::class,
+    ],
     'dsn' => 'mysql:host=db;dbname=yii2basic',
     'username' => 'root',
     'password' => 'example',
